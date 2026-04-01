4e74db759c1dfc27cc9164234b49becbd529c663
Honounome <wesleybsousa@hotmail.com>
2026-04-01 17:44:53 -0300

    arquivos base adicionados

---
+++ crud.c
@@ -0,0 +1,81 @@
+#include "crud.h"
+
+struct _cabecalho {
+    char status[1]; // 0 ou 1, indicando se o arquivo está consistente ou inconsistente
+    int topo; // byte offset do topo da pilha de registros removidos
+    int proxRRN; // próximo RRN a ser inserido
+    int nroEstacoes; // número de estações cadastradas
+    int nroPares; // número de pares de estações cadastrados
+};
+
+struct _dados {
+    char removido[1];
+    char proximo[4];
+    int codEstacao;
+    int codLinha;
+    int codProxEstacao;
+    int distProxEstacao;
+    int codLinhaIntegra;
+    int codEstIntegra;
+    int tamNomeEstacao;
+    char* nomeEstacao;
+    int tamNomelinha;
+    char* nomeLinha;
+};
+
+
+
+void BinarioNaTela(char *arquivo) {
+    FILE *fs;
+    if (arquivo == NULL || !(fs = fopen(arquivo, "rb"))) {
+        fprintf(stderr,
+                "ERRO AO ESCREVER O BINARIO NA TELA (função binarioNaTela): "
+                "não foi possível abrir o arquivo que me passou para leitura. "
+                "Ele existe e você tá passando o nome certo? Você lembrou de "
+                "fechar ele com fclose depois de usar?\n");
+        return;
+    }
+
+    fseek(fs, 0, SEEK_END);
+    size_t fl = ftell(fs);
+
+    fseek(fs, 0, SEEK_SET);
+    unsigned char *mb = (unsigned char *)malloc(fl);
+    fread(mb, 1, fl, fs);
+
+    unsigned long cs = 0;
+    for (unsigned long i = 0; i < fl; i++) {
+        cs += (unsigned long)mb[i];
+    }
+
+    printf("%lf\n", (cs / (double)100));
+
+    free(mb);
+    fclose(fs);
+}
+
+void ScanQuoteString(char *str) {
+    char R;
+
+    while ((R = getchar()) != EOF && isspace(R))
+        ; // ignorar espaços, \r, \n...
+
+    if (R == 'N' || R == 'n') { // campo NULO
+        getchar();
+        getchar();
+        getchar();       // ignorar o "ULO" de NULO.
+        strcpy(str, ""); // copia string vazia
+    } else if (R == '\"') {
+        if (scanf("%[^\"]", str) != 1) { // ler até o fechamento das aspas
+            strcpy(str, "");
+        }
+        getchar();         // ignorar aspas fechando
+    } else if (R != EOF) { // vc tá tentando ler uma string que não tá entre
+                           // aspas! Fazer leitura normal %s então, pois deve
+                           // ser algum inteiro ou algo assim...
+        str[0] = R;
+        scanf("%s", &str[1]);
+    } else { // EOF
+        strcpy(str, "");
+    }
+}
